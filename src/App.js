739eea530ba964d05629c7e570a64287ce7e2381
seanmcannon <sean.m.cannon@gmail.com>
2020-09-11 11:29:59 -0700

    met mvp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+
+
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+  const URL = `https://swapi.dev/api/people/`
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+  useEffect(() => {
+    axios.get(URL)
+      .then(response => {
+        setCharacters(response.data.results);
+        console.log(response.data.results)
+      })
+      .catch(err => console.log('Error: ', err))
+      
+  }, [])
+
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
+      <div className="App">
+        <h1>Star Wars Characters</h1>
+        
+    <   Character characters={characters} />
+      </div>
+    );
+    }
 
-export default App;
+
+
+
+   export default App
+
+
---
+++ src/theme/index.js
@@ -0,0 +1,14 @@
+export default {
+    white: 'white',
+    black: 'black',
+    gold: 'darkgoldenrod',
+    margins: ' 3%',
+    paddings: '1%',
+   
+    }
+  
+
+
+
+
+
